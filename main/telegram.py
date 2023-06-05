0bc97307ff584ae7e6a6bc86b45fffb1f19549e5
RiderMC <68037751+RiderMC126@users.noreply.github.com>
2023-06-05 21:24:46 +0300

    telegram.py

--- main/telegram.py
+++ main/telegram.py
@@ -4,15 +4,15 @@ from . import templates
 from .models import Task
 from django.shortcuts import render
 
-bot = telebot.TeleBot(token='6165128390:AAG1Rseq1CJOOZRqbWNf-bDduFwfS-QIPbY')
+bot = telebot.TeleBot(token='') # here is your telegram token
 
 
 def send(request):
     name = request.POST.get('name')
 
-    admin_id = 502447701
+    admin_id = 123456789    # here is your telegram id
     message = f"ЗАЯВКА!!\n\nОт {name}"
-    bot.send_message(chat_id=admin_id, text=message)
+    bot.send_message(chat_id=admin_id, text=message)    
 
 
 
